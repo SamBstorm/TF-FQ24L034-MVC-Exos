40c9119fc2bebdcef25996574b45fc749aa0405c
Samuel Legrain <samuel.legrain@bstorm.be>
2024-12-18 09:56:01 +0100

    Ajoutez des fichiers projet.

---
+++ ASPMVC/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPMVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(Login));
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
